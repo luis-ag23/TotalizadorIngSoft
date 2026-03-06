c5cd0ff14bc4067ed9b98580e1fa26b2fd2cb8ec
BigSmoke <cesarcadiz26@gmail.com>
2026-03-06 19:42:33 -0400

    test: se añadio el file para poder hacer la modificacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,1 +1,13 @@
+import precioNeto from "./totalizador.js";
 
+const cantidadItemsInput = document.querySelector(".inp-cantItems");
+const precioItemInput = document.querySelector(".inp-priceItems");
+const calcularButton = document.querySelector(".btn-calcular");
+const precioNetoValor = document.querySelector("#precio-neto-valor");
+
+calcularButton.addEventListener("click", () => {
+	const cantidadItems = Number.parseFloat(cantidadItemsInput.value) || 0;
+	const precioItem = Number.parseFloat(precioItemInput.value) || 0;
+
+	precioNetoValor.textContent = String(precioNeto(cantidadItems, precioItem));
+});
